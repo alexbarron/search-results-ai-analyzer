268ecee900cb0af857d43759c9ad73452489eb51
Alex Barron <alex@abarron.com>
2024-01-23 11:30:22 +0100

    Add OpenAI service

---
+++ app/services/openai_services/chat_service.rb
@@ -0,0 +1,39 @@
+module OpenAIServices
+  class ChatService
+    attr_reader :search_analysis
+    
+    def initialize(search_analysis)
+      @search_analysis = search_analysis
+    end
+    
+    def call
+      begin
+        client = OpenAI::Client.new
+        response = client.chat(
+          parameters: {
+              model: "gpt-3.5-turbo",
+              messages: [{ role: "user", content: generate_prompt}],
+              temperature: 0,
+          })
+        chat_response = response.dig("choices", 0, "message", "content")
+      rescue StandardError => e
+        OpenStruct.new(success?: false, error: e)
+      else
+        OpenStruct.new({success?: true, openai_response: chat_response})
+      end
+    end
+    
+    private
+    
+      def generate_prompt
+        <<~PROMPT
+          #{@search_analysis.prompt}
+
+          My search query: #{@search_analysis.query}"
+
+          The search results were:
+          #{@search_analysis.search_results}
+        PROMPT
+      end
+  end
+end
